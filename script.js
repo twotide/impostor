10b1ae3f1388242498854c336f48f4354dc08b45
twotide <telepaze@gmai.com>
2026-02-12 20:29:41 -0500

    font change, added round counter

--- script.js
+++ script.js
@@ -13,24 +13,28 @@ function start()
     impostorWord = document.getElementById("input1").value;
     normalWord = document.getElementById("input2").value;
 
-    players = document.getElementById("playerInput").value;
-    impostorId = Math.floor(Math.random() * players);
+    players = parseInt(document.getElementById("playerInput").value);
+    impostorId = Math.floor(Math.random() * (players - 1));
 
     round = 0;
+
+    document.getElementById("input1").value = "hidden";
+    document.getElementById("input2").value = "hidden";
 }
 
 function nextWord() 
 {
     if (round === impostorId)
     {
-        wordText.textContent = impostorWord;
+        wordText.textContent = (impostorWord + " (player " + (round + 1) + "/" + players + ")");
     }
     else
     {
-        wordText.textContent = normalWord;
+        wordText.textContent = (normalWord + " (player " + (round + 1) + "/" + players + ")");
     }
 
-    round++;
+    if (round >= players) wordText.textContent = "all words are done"; 
+    else round++;
 }
 
 function hideWord()
